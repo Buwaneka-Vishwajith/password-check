aeca2964c1b15dcc2fffd84fbb52af62820ad74f
Buwaneka <buwaneka10000@gmail.com>
2024-12-16 18:42:31 +0530

    first

---
+++ src/components/Form.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <div className="bg-blue-300 flex flex-col items-center p-6">
+      <h1 className="text-3xl mb-4">Password Checker</h1>
+      <form className="mb-4">
+        <label className='text-[20px]'>Password </label>
+        <input
+          type="text"
+          className="border border-blue-500 p-2 rounded-[10px]"
+          placeholder="Enter your password"
+        />
+      </form>
+
+        <div className="flex flex-row w-full">
+        <div className="w-1/2 bg-red-400 h-[50px] "></div>
+        <div className="w-1/2 bg-green-400 h-[50px]"></div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Form
