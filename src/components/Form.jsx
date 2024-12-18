952cc1f64082634a716e153ad4ff982a5752b382
Buwaneka <buwaneka10000@gmail.com>
2024-12-18 13:23:22 +0530

    bubbles added

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import p from "../assets/p.png"
+import { ParallaxProvider } from 'react-scroll-parallax';
 
 const Form = () => {
 
@@ -8,6 +10,21 @@ const Form = () => {
     numbers: false,
     special: false,
   });
+
+  const [bubbles , setBubbles] = useState ([]);
+
+  useEffect(() => {
+    generateBubbles();
+  }, []);
+
+  const generateBubbles = () => {
+    const bubbleArray = Array.from({length: 20}).map(() => ({
+      left: Math.random() * 100,
+      top: Math.random() * 100,
+      size: Math.random() * 100 + 10,
+    }));
+    setBubbles(bubbleArray);
+  };
 
   const handlePasswordChange = (e) => {
     const newPasword = e.target.value;
@@ -40,13 +57,32 @@ const Form = () => {
   const strengthLevel = getStrengthlevel(strength);
 
   return (
-    <div className="bg-color-8 flex justify-center items-center h-screen">
-      <div className="flex flex-col p-6 bg-color-1 rounded-xl shadow-lg w-1/3">
+    <div className="relative w-screen overflow-hidden bg-color-8 flex justify-center items-center h-screen">
+      
+      {bubbles.map((bubble, index) => (
+        <div
+          key={index}
+          className="absolute rounded-full z-0"
+          style={{
+            width: `${bubble.size}px`,
+            height: `${bubble.size}px`,
+            backgroundImage: `url(${p})`,
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            top: `${bubble.top}%`,
+            left: `${bubble.left}%`,
+            opacity: 0.8,
+            zIndex: 0,
+          }}
+        ></div>
+      ))}
+
+      <div className="flex flex-col p-6 bg-color-1 rounded-xl z-10 shadow-lg w-1/3">
         <h1 className="text-3xl mb-4 text-color-5 flex justify-center">Password Strength Checker</h1><br/>
         
         <form className="mb-4">
           <input
-            type="text"
+            type="password"
             className="border border-blue-500 p-2 rounded-[10px] w-full "
             placeholder="Enter your password"
             value={password}
