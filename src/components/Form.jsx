70952e78305cf839c1979e34164b713e9d7a38a2
Buwaneka <buwaneka10000@gmail.com>
2024-12-19 12:14:28 +0530

    bubbles go up!

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,43 +1,39 @@
-import React, { useEffect, useState } from 'react'
-import p from "../assets/p.png"
-import green from "../assets/green.png"
-import red from "../assets/red.png"
-import yellow from "../assets/yello.png"
-import { ParallaxProvider } from 'react-scroll-parallax';
-import { MouseParallaxContainer, MouseParallaxChild } from "react-parallax-mouse";
+import React, { useEffect, useState } from 'react';
+import green from "../assets/green.png";
+import red from "../assets/red.png";
+import yellow from "../assets/yello.png";
 
 const Form = () => {
-
-  const [password , setPassword] = useState("");
-  const [strength , setStrength] = useState({
+  const [password, setPassword] = useState("");
+  const [strength, setStrength] = useState({
     length: false,
     numbers: false,
     special: false,
   });
 
-  const [bubbles , setBubbles] = useState ([]);
+  const [bubbles, setBubbles] = useState([]);
 
   useEffect(() => {
     generateBubbles();
   }, []);
 
   const generateBubbles = (strengthLevel) => {
-    const bubbleArray = Array.from({length: 20}).map(() => ({
+    const bubbleArray = Array.from({ length: 20 }).map(() => ({
       left: Math.random() * 100,
-      top: Math.random() * 100,
       size: Math.random() * 150 + 50,
       color: getBubbleColor(strengthLevel),
+      animationDuration: Math.random() * 5 + 5, // 5 to 10 seconds
+      animationDelay: Math.random() * 2, // 0 to 2 seconds
     }));
     setBubbles(bubbleArray);
   };
 
   const getBubbleColor = (strengthLevel) => {
-
-    if (strengthLevel < 2){
+    if (strengthLevel < 2) {
       return red;
-    }else if (strengthLevel === 2){
+    } else if (strengthLevel === 2) {
       return yellow;
-    }else{
+    } else {
       return green;
     }
   };
@@ -45,7 +41,6 @@ const Form = () => {
   const handlePasswordChange = (e) => {
     const newPasword = e.target.value;
     setPassword(newPasword);
-    // setStrength(checkPasswordStrength(newPasword));
     const newStrength = checkPasswordStrength(newPasword);
     setStrength(newStrength);
 
@@ -68,9 +63,9 @@ const Form = () => {
   };
 
   const getProgressbarColor = (level) => {
-      if (level === 3) return "bg-color-6";
-      if (level === 2) return "bg-color-9";
-      if (level < 2) return "bg-color-7";
+    if (level === 3) return "bg-color-6";
+    if (level === 2) return "bg-color-9";
+    if (level < 2) return "bg-color-7";
   };
 
   const getLabelColor = (isValid) => {
@@ -80,19 +75,10 @@ const Form = () => {
   const strengthLevel = getStrengthlevel(strength);
 
   return (
-    <MouseParallaxContainer className="relative w-screen overflow-hidden bg-color-8 
-                                       flex justify-center items-center h-screen"
-                            globalFactorX={0.1}  
-                            globalFactorY={0.1}         
-                            >
-                            
-      
+    <div className="relative w-screen overflow-hidden bg-color-8 flex justify-center items-center h-screen">
       {bubbles.map((bubble, index) => (
-        <MouseParallaxChild
+        <div
           key={index}
-          factorX={0.5 }
-          factorY={0.2 }
-      
           className="absolute rounded-full z-0"
           style={{
             width: `${bubble.size}px`,
@@ -100,20 +86,23 @@ const Form = () => {
             backgroundImage: `url(${bubble.color})`,
             backgroundSize: "cover",
             backgroundRepeat: "no-repeat",
-            top: `${bubble.top}%`,
             left: `${bubble.left}%`,
-            opacity: 0.8,
-            // zIndex: 0,
+            bottom: "0%", // Start at the bottom
+            animation: `floatUp ${bubble.animationDuration}s ease-in-out infinite`,
+            animationDelay: `${bubble.animationDelay}s`,
           }}
-            
-        ></MouseParallaxChild>
+        ></div>
       ))}
-      
 
-      <div className="flex flex-col p-6 bg-color-1 bg-opacity-30 backdrop-blur-md border
-                     border-color-1 rounded-xl z-10 shadow-lg lg:w-1/3 sm:w-2/3 md:w-1/2">
-        <h1 className="text-3xl mb-4 text-color-5 flex justify-center">Password Strength Meter</h1><br/>
-        
+      <div
+        className="flex flex-col p-6 bg-color-1 bg-opacity-30 backdrop-blur-md border
+                     border-color-1 rounded-xl z-10 shadow-lg lg:w-1/3 sm:w-2/3 md:w-1/2"
+      >
+        <h1 className="text-3xl mb-4 text-color-5 flex justify-center">
+          Password Strength Meter
+        </h1>
+        <br />
+
         <form className="mb-4">
           <input
             type="password"
@@ -121,23 +110,56 @@ const Form = () => {
             placeholder="Enter your password"
             value={password}
             onChange={handlePasswordChange}
-          /><br/><br/>
-          
-        <div className="flex flex-row w-full">
-          <div 
-          className={` h-[10px] rounded-lg ${getProgressbarColor(strengthLevel)}`}
-          style={{width: password ? `${(strengthLevel / 3) * 100}%` : `2%`, transition: "width 0.3s ease-in-out" }}
-          ></div>
-        </div><br/>
+          />
+          <br />
+          <br />
 
-          <label className={`text-[20px] ${getLabelColor(strength.length)}`}>8 characters </label><br/>
-          <label className={`text-[20px] ${getLabelColor(strength.numbers)}`}>Numbers </label><br/>
-          <label className={`text-[20px] ${getLabelColor(strength.special)}`}>Special characters </label><br/>
+          <div className="flex flex-row w-full">
+            <div
+              className={` h-[10px] rounded-lg ${getProgressbarColor(
+                strengthLevel
+              )}`}
+              style={{
+                width: password
+                  ? `${(strengthLevel / 3) * 100}%`
+                  : `2%`,
+                transition: "width 0.3s ease-in-out",
+              }}
+            ></div>
+          </div>
+          <br />
+
+          <label className={`text-[20px] ${getLabelColor(strength.length)}`}>
+            At least 8 characters{" "}
+          </label>
+          <br />
+          <label className={`text-[20px] ${getLabelColor(strength.numbers)}`}>
+            At least one number{" "}
+          </label>
+          <br />
+          <label className={`text-[20px] ${getLabelColor(strength.special)}`}>
+            At least one special character{" "}
+          </label>
+          <br />
         </form>
-
       </div>
-    </MouseParallaxContainer>
-  )
-}
+
+      <style>
+        {`
+          @keyframes floatUp {
+            0% {
+              transform: translateY(0);
+              opacity: 1;
+            }
+            100% {
+              transform: translateY(-200vh);
+              opacity: 0;
+            }
+          }
+        `}
+      </style>
+    </div>
+  );
+};
 
 export default Form;
